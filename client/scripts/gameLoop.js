65d090738404e25e57c4d3cdb38e06ff2133e199
Oxfam <louiscdb@icloud.com>
2025-09-21 18:54:24 +0200

    Fix import path case for Linux build

--- client/scripts/gameLoop.js
+++ client/scripts/gameLoop.js
@@ -5,7 +5,7 @@ import { npc } from './npc.js';
 import { Chest } from './chest.js';
 import { toVec3 } from './networkclient.js';
 import { UI } from './UIclient.js';
-import { loadLevel } from '../leveleditor/loadlevel.js';
+import { loadLevel } from '../levelEditor/loadlevel.js';
 import { levelHandler } from './levelHandler.js';
 import {vfxHandler} from "./vfxHandeler.js";
 
